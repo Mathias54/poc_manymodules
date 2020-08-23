fc25061f9d9ea50076aa009c5dc510f28146ef32
Mathias Azzolini <mathiasa@dbserver.com.br>
2020-08-23 02:06:48 -0300

    feat: adiciona modulo one

---
+++ README.md
@@ -0,0 +1,3 @@
+# Many
+
+This project was as goal create one architecture where many teams can build one application without have problems to publish theirs modules.
--- packages/one/src/index.js
+++ packages/one/src/index.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import {version} from '../package.json';
 
 function One() {
   return (
-    <div>Módulo 1</div>
+    <div>Módulo 1. Versão: {version}</div>
   )
 }
 
